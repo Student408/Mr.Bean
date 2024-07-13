0f40d613a269b81792357258b0743139c583ddf0
Ranjan Shettigar <158475083+Student408@users.noreply.github.com>
2024-07-14 02:17:38 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,8 @@
-export const USER_ID = 'ihgNNR31qVbzmD1Kt';
-export const SERVICE_ID = 'service_w98dyda';
-export const TEMPLATE_ID = 'template_pj5lu5p';
+// export const USER_ID = 'ihgNNR31qVbzmD1Kt';
+// export const SERVICE_ID = 'service_w98dyda';
+// export const TEMPLATE_ID = 'template_pj5lu5p';
+
+
+export const USER_ID = process.env.REACT_APP_USER_ID;
+export const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+export const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
